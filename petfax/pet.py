73b9786ac1d0a530cdbbbaee064bba4e14c9b6f4
Alex Schmidt <alex.schmidt767@gmail.com>
2023-01-24 18:03:37 -0600

    route for info page and links to info pages set up

--- petfax/pet.py
+++ petfax/pet.py
@@ -7,3 +7,11 @@ pets = json.load(open('pets.json'))
 @bp.route('/')
 def index():
     return render_template('index.html', pets=pets)
+
+@bp.route('/<pet_id>')
+def info(pet_id):
+    found_pet = None
+    for pet in pets:
+        if pet.pet_id is pet_id:
+            found_pet = pet
+    return render_template('info.html', pet=found_pet)
