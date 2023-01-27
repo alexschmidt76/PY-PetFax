96a9ffec41d1d303d9d0893276fef63b38f75f06
Alex Schmidt <alex.schmidt767@gmail.com>
2023-01-26 19:24:44 -0600

    flask-migrate migration tools configured

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,15 +1,17 @@
+# config
 from flask import Flask
+from flask_migrate import Migrate
 
 # factory
 def create_app():
     app = Flask(__name__)
 
-    # sqlalchemy config
+    # sqlalchemy database config
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Aborted5732@localhost:5432/petfax'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
     from . import models
     models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
     # landing page
     @app.route('/')
