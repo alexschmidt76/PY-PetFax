942fbb3d00456221c69e122a0d09ceb566cd4af1
Alex Schmidt <alex.schmidt767@gmail.com>
2023-01-26 19:16:48 -0600

    sql db connection initialized

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -4,6 +4,13 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    # sqlalchemy config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Aborted5732@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+
     # landing page
     @app.route('/')
     def hello():
